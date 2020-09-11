109583cd43a5e67917311d9f9b3ca16261a5f600
Mikel_Pan <plyx_46204@126.com>
2020-09-11 16:29:53 +0800

    添加推送企业微信

--- src/main/java/site/plyx/yunfeigjdevops/YunfeigjDevopsApplication.java
+++ src/main/java/site/plyx/yunfeigjdevops/YunfeigjDevopsApplication.java
@@ -3,11 +3,11 @@ package site.plyx.yunfeigjdevops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class YunfeigjDevopsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(YunfeigjDevopsApplication.class, args);
 	}
-
 }
